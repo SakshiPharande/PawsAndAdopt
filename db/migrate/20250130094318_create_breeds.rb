12fb222728dc880605e283656d52e358c06db4c2
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-01-30 15:48:53 +0530

    Revert "create database"
    This reverts commit 2267eced69689d3a1622e73f1334592904c2a661.

--- db/migrate/20250130094318_create_breeds.rb
+++
@@ -1,9 +0,0 @@
-class CreateBreeds < ActiveRecord::Migration[7.2]
-  def change
-    create_table :breeds do |t|
-      t.string :breed_name, null: false
-      t.references :category, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
