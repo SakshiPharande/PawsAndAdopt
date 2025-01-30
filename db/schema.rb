7746664d4b631b3d9ad2c9d8e1afd94c6221620f
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-01-30 17:55:54 +0530

    add device gem and removed password_digest coloumn from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_30_103737) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_30_122342) do
   create_table "breeds", force: :cascade do |t|
     t.string "breed_name", null: false
     t.integer "category_id", null: false
@@ -78,11 +78,20 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_103737) do
     t.string "profile_img_url"
     t.string "email", null: false
     t.string "phone_no"
-    t.string "password_digest"
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "breeds", "categories"
