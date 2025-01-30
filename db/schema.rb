1b339bfc60dce2fdabce371fe2c0632393877e44
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-01-30 15:55:04 +0530

    add breed model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_30_102144) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_30_102305) do
+  create_table "breeds", force: :cascade do |t|
+    t.string "breed_name", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_breeds_on_category_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -29,4 +37,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_102144) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "breeds", "categories"
 end
