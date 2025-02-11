d82ad2de7b5ba613fba9d1388e8213899a3f921e
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-11 16:24:09 +0530

    add pet images and preview them admin side

--- app/controllers/admin/pets_controller.rb
+++ app/controllers/admin/pets_controller.rb
@@ -14,7 +14,10 @@ class Admin::PetsController < ApplicationController
 
   def create
     @pet = Pet.new(pet_params)
+
     if @pet.save
+      Rails.logger.debug "Uploaded images: #{params[:pet][:pet_images].inspect}"
+      attach_images(@pet)
       redirect_to admin_pets_path, notice: "Pet successfully added."
     else
       load_categories_and_breeds
@@ -30,14 +33,20 @@ class Admin::PetsController < ApplicationController
 
   def update
     @pet = Pet.find(params[:id])
+    # Remove images if any are marked for deletion
+    if params[:pet][:removed_images].present?
+      params[:pet][:removed_images].each do |image_id|
+        @pet.pet_images.find(image_id).purge
+      end
+    end
+
     if @pet.update(pet_params)
-      redirect_to admin_pets_path, notice: "Pet successfully updated."
+      redirect_to admin_pets_path, notice: "Pet updated successfully!"
     else
-      load_categories_and_breeds
-      flash.now[:alert] = @pet.errors.full_messages.to_sentence
       render :edit
     end
   end
+
 
 
   def discard
@@ -64,9 +73,14 @@ class Admin::PetsController < ApplicationController
   def pet_params
     params.require(:pet).permit(:age, :gender, :temperament, :vaccination_status,
                                 :medical_history, :recommended_food, :common_health_issues,
-                                :status, :breed_id, :category_id).tap do |pet_params|
+                                :status, :breed_id, :category_id, pet_images: []).tap do |pet_params|
       pet_params[:gender] = pet_params[:gender].to_i if pet_params[:gender].present?
       pet_params[:status] = pet_params[:status].to_i if pet_params[:status].present?
     end
+  end
+
+  def attach_images(pet)
+    return unless params[:pet][:pet_images].present?
+    pet.pet_images.attach(params[:pet][:pet_images])
   end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,9 +3,16 @@ class Pet < ApplicationRecord
   belongs_to :breed
   belongs_to :category
 
+  has_many_attached :pet_images
+
   enum gender: { male: 1, female: 2, unknown: 0 }
   enum status: { available: 0, unavailable: 1 }
 
   validates :category_id, :breed_id, :age, :gender, :temperament, :status, presence: true
   validates :vaccination_status, inclusion: { in: [ true, false ] }
+
+  # Method to return multiple image URLs
+  def pet_images_urls
+    pet_images.map { |image| Rails.application.routes.url_helpers.rails_blob_url(image, only_path: true) }
+  end
 end
