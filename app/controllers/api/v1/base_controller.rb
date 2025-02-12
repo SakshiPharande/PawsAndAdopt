dd82f35903a4f357c846f8577c83eda0eed871cd
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-12 20:33:58 +0530

    update auth_controller and base_controller use rescue_form for handelig errors in json

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::BaseController < ActionController::API
   include JwtHelper
 
   # skip_before_action :verify_authenticity_token  # Disable CSRF for API requests
-  before_action :authenticate_user
+  before_action :authenticate_user!
+
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from StandardError, with: :internal_server_error
 
   private
 
@@ -13,8 +17,19 @@ class Api::V1::BaseController < ActionController::API
 
     if decoded
       @current_user = User.find_by(id: decoded[:user_id])
+      render json: { error: "Unauthorized: Please login first" }, status: :unauthorized unless @current_user
     else
       render json: { error: "Unauthorized: Please login first" }, status: :unauthorized
     end
+  end
+
+  # Handle Record Not Found (e.g., when querying a non-existent record)
+  def record_not_found(exception)
+    render json: { success: false, error: "Record not found: #{exception.message}" }, status: :not_found
+  end
+
+  # Handle Other Unexpected Errors
+  def internal_server_error(exception)
+    render json: { success: false, error: "Something went wrong: #{exception.message}" }, status: :internal_server_error
   end
 end
