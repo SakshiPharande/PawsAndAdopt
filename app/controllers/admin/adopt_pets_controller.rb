8e2749e2439d912a13d70de0b21226a68923fe3a
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-13 17:47:53 +0530

    feature to change actual-adoption-date admin side

--- app/controllers/admin/adopt_pets_controller.rb
+++ app/controllers/admin/adopt_pets_controller.rb
@@ -8,20 +8,33 @@ class Admin::AdoptPetsController < ApplicationController
   end
 
   def update
-    @adopt_pet = AdoptPet.find(params[:id])
+    @adopt_pet = AdoptPet.find(params[:id])  # Ensure @adopt_pet is set
 
-    if params[:status].present? && AdoptPet.statuses.keys.include?(params[:status])
-      @adopt_pet.update(status: params[:status])
-
+    if params[:adopt_pet] && @adopt_pet.update(adopt_pet_params)
+      @index = @adopt_pet.id
       respond_to do |format|
-        format.html { redirect_to admin_adopt_pets_path, notice: "Adopt request has been #{params[:status]} successfully." }
+        format.html { redirect_to admin_adopt_pets_path, notice: "Adopt request updated successfully." }
         format.turbo_stream
       end
+
+    elsif params[:status].present? && AdoptPet.statuses.keys.include?(params[:status])
+      if @adopt_pet.update(status: params[:status])
+        @index = @adopt_pet.id
+        respond_to do |format|
+          format.html { redirect_to admin_adopt_pets_path, notice: "Adopt request has been #{params[:status]} successfully." }
+          format.turbo_stream
+        end
+      else
+        flash[:error] = "Status update failed."
+        redirect_to admin_adopt_pets_path
+      end
+
     else
-      flash[:error] = "Invalid status update."
+      flash[:error] = "Invalid update request."
       redirect_to admin_adopt_pets_path
     end
   end
+
 
 
   private
@@ -32,5 +45,9 @@ class Admin::AdoptPetsController < ApplicationController
       flash[:error] = "Adoption request not found."
       redirect_to admin_adopt_pets_path
     end
+  end
+
+  def adopt_pet_params
+    params.require(:adopt_pet).permit(:actual_adoption_date, :status)
   end
 end
