281cb711d218da438be6418db17642507231f69f
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-16 09:51:29 +0530

    add bredcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,34 @@ module ApplicationHelper
       "alert-info"
     end
   end
+
+  def breadcrumbs
+    content_tag(:nav, aria: { label: "breadcrumb" }) do
+      content_tag(:ol, class: "breadcrumb p-2 rounded bg-light") do
+        links = []
+        links << breadcrumb_item("Dashboard", admin_dashboard_path)
+
+        pages = {
+          "users" => "Users",
+          "categories" => "Categories",
+          "breeds" => "Breeds",
+          "pets" => "Pets",
+          "donate_pets" => "Donate Pet Requests",
+          "adopt_pets" => "Adopt Pet Requests"
+        }
+
+        links << breadcrumb_item(pages[controller_name], send("admin_#{controller_name}_path")) if pages[controller_name]
+
+        links << content_tag(:li, action_name.humanize, class: "breadcrumb-item h5 mb-0 active", style: "color: rgb(105, 108, 255); text-decoration:none", aria: { current: "page" }) if action_name != "index"
+
+        links.join.html_safe
+      end
+    end
+  end
+
+  private
+
+  def breadcrumb_item(name, path)
+    content_tag(:li, link_to(name, path, class: "h5 mb-0 text-decoration-none", style: "color: rgb(105, 108, 255);"), class: "breadcrumb-item")
+  end
 end
