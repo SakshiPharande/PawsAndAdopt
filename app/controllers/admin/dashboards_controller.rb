921fb597078ea2e345fc5632d78a6831965eb99e
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-16 19:29:11 +0530

    add counts to admin-dashboard and change favicon of project

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,8 +2,13 @@ class Admin::DashboardsController < ApplicationController
   before_action :authenticate_user!  # Ensures user is logged in
   before_action :ensure_admin  # Ensures user is an admin
 
-   def index
-   end
+  def index
+    @user_count = User.kept.where(role: "user").count
+    @category_count = Category.kept.count
+    @breed_count = Breed.kept.count
+    @pet_count = Pet.kept.count
+  end
+
   # Check if the current user is an admin
   def ensure_admin
     unless current_user.admin? || current_user.super_admin?
