4ae5289e96e7ea512d89041331801a2edb9aa7bd
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-17 18:20:04 +0530

    add pagignation to categories admin-side

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.kept
+    @categories = Category.kept.paginate(page: params[:page], per_page: 5)
   end
 
   def show
