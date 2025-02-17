6a551f824a3df765fafcf550891049490afcb51e
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-17 18:10:12 +0530

    add helper function for pagignation and use it in pets and users view to show pagignation for listing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   def index
-    @users = User.kept.where(role: "user")
+    @users = User.kept.where(role: "user").paginate(page: params[:page], per_page: 5)
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,39 @@ module ApplicationHelper
     end
   end
 
+
+  def pagination_links(collection)
+    content_tag(:div, class: "d-flex justify-content-center mt-3") do
+      content_tag(:nav, aria: { label: "Page navigation" }) do
+        content_tag(:ul, class: "pagination pagination-sm") do
+          previous_link = if collection.previous_page
+                            link_to("&laquo;".html_safe, url_for(page: collection.previous_page), class: "page-link", aria: { label: "Previous" })
+          else
+                            content_tag(:span, "&laquo;".html_safe, class: "page-link")
+          end
+
+          next_link = if collection.next_page
+                        link_to("&raquo;".html_safe, url_for(page: collection.next_page), class: "page-link", aria: { label: "Next" })
+          else
+                        content_tag(:span, "&raquo;".html_safe, class: "page-link")
+          end
+
+          pagination_links = (1..collection.total_pages).map do |page_number|
+            active_style = page_number == collection.current_page ? "background-color: rgb(105, 108, 255); color: white; border-color: rgb(105, 108, 255);" : ""
+            content_tag(:li, class: "page-item #{'active' if page_number == collection.current_page}") do
+              link_to page_number, url_for(page: page_number), class: "page-link", style: active_style
+            end
+          end
+
+          content_tag(:li, previous_link, class: "page-item #{'disabled' unless collection.previous_page}") +
+          safe_join(pagination_links) +
+          content_tag(:li, next_link, class: "page-item #{'disabled' unless collection.next_page}")
+        end
+      end
+    end
+  end
+
+
   def breadcrumbs
     content_tag(:nav, aria: { label: "breadcrumb" }) do
       content_tag(:ol, class: "breadcrumb p-2 rounded bg-light") do
