1747382953e31e2ebd9af756d8746e62a43e33df
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-18 20:14:36 +0530

    partial bug fix for update status adopt-pet-request

--- app/controllers/admin/adopt_pets_controller.rb
+++ app/controllers/admin/adopt_pets_controller.rb
@@ -9,35 +9,48 @@ class Admin::AdoptPetsController < ApplicationController
 
   def update
     @adopt_pet = AdoptPet.find(params[:id])  # Ensure @adopt_pet is set
+    @adopt_pet_index = params[:index].to_i if params[:index].present?
 
-    if params[:adopt_pet] && @adopt_pet.update(adopt_pet_params)
-      @index = @adopt_pet.id
-      respond_to do |format|
-        format.html { redirect_to admin_adopt_pets_path, notice: "Adopt request updated successfully." }
-        format.turbo_stream
-      end
-
+    if params[:adopt_pet]
+      update_adopt_pet_date
     elsif params[:status].present? && AdoptPet.statuses.keys.include?(params[:status])
-      if @adopt_pet.update(status: params[:status])
-        @index = @adopt_pet.id
-        respond_to do |format|
-          format.html { redirect_to admin_adopt_pets_path, notice: "Adopt request has been #{params[:status]} successfully." }
-          format.turbo_stream
-        end
-      else
-        flash[:error] = "Status update failed."
-        redirect_to admin_adopt_pets_path
-      end
-
+      update_adopt_pet_status
     else
       flash[:error] = "Invalid update request."
       redirect_to admin_adopt_pets_path
     end
   end
 
-
-
   private
+
+
+  # Method to update actual adoption date
+  def update_adopt_pet_date
+    if @adopt_pet.update(adopt_pet_params)
+      @index = @adopt_pet.id
+      respond_to do |format|
+        format.html { redirect_to admin_adopt_pets_path, notice: "Adoption date updated successfully." }
+        format.turbo_stream
+      end
+    else
+      flash[:error] = "Failed to update adoption date."
+      redirect_to admin_adopt_pets_path
+    end
+  end
+
+  # Method to update pet status
+  def update_adopt_pet_status
+    if @adopt_pet.update(status: params[:status])
+      @index = @adopt_pet.id
+      respond_to do |format|
+        format.html { redirect_to admin_adopt_pets_path, notice: "Adopt request has been #{params[:status]} successfully." }
+        format.turbo_stream
+      end
+    else
+      flash[:error] = "Status update failed."
+      redirect_to admin_adopt_pets_path
+    end
+  end
 
   def set_adopt_pet
     @adopt_pet = AdoptPet.find_by(id: params[:id])
