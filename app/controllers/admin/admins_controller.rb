38addc42eff6c8ce42a5bea4258f54f34bac137c
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-21 18:29:16 +0530

    edit and show admin profile on admin dashboard

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,28 +1,39 @@
 class Admin::AdminsController < ApplicationController
+  before_action :authorize_admin # Ensure only admins can edit their profiles
+
   def show
     @admin = current_user
-    unless @admin.admin?
-      redirect_to root_path, alert: "You are not authorized to view this page."
-    end
   end
 
   def edit
-    @admin = User.find(params[:id]).where(role: "admin")
+    @admin = current_user
   end
 
   def update
-    @admin = User.find(params[:id]).where(role: "admin")
-    # Remove images if any are marked for deletion
-    if params[:pet][:removed_images].present?
-      params[:pet][:removed_images].each do |image_id|
-        @pet.pet_images.find(image_id).purge
-      end
-    end
+    @admin = current_user
 
-    if @pet.update(pet_params)
+    if @admin.update(admin_params)
+      # Handle profile image update
+      if params[:user][:profile_image].present?
+        @admin.profile_image.purge # Remove existing profile image before uploading a new one
+        @admin.profile_image.attach(params[:user][:profile_image])
+      end
+
       redirect_to admin_admins_path, notice: "Profile updated successfully!"
     else
       render :edit
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_no, :profile_image)
+  end
+
+  def authorize_admin
+    unless current_user.admin?
+      redirect_to root_path, alert: "You are not authorized to perform this action."
     end
   end
 end
