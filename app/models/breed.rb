a6cf59b2cb1376b5bebf9f1f71790307102c95e3
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-02-26 01:08:12 +0530

    add validations to breed-form at admin side

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -2,5 +2,7 @@ class Breed < ApplicationRecord
   include Discard::Model
   belongs_to :category
   has_many :pets
-  validates :breed_name, presence: true
+
+  validates :breed_name, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
+  validates :category_id, presence: true
 end
